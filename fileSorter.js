d770e8de90d916db6a1bbf81240ddacebc0fc9a8
svitlarp <svitlanka.ripa@gmail.com>
2025-05-20 14:06:58 +0200

    remove comments, unnecessary code

--- fileSorter.js
+++ fileSorter.js
@@ -5,44 +5,6 @@ const path = require('path');
 const filename = process.argv[2];
 
 
-if (!filename) {
-    throw Error('A file to sort must be specified!')  // 1 Handle if any file specified
-}
-if (!fs.existsSync(filename)) {
-    throw Error("Given path doesn't exists. Check the path");  // 2 Check if this path exists
-}
-console.log(`Given file is: ${filename}`);
-
-
-
-function handleFile(file) {
-    // const stat = fs.statSync(file);
-    // console.log(stat.isBlockDevice(), stat.isDirectory(), stat.size);
-    // const ext = filename.split(".").at(-1);
-    const ext = path.extname(file).replace('.', '');
-    console.log(ext);
-    if (fs.existsSync(ext)) {
-        console.log(`Directory: "${ext}" already exists`);
-    } else {
-        fs.mkdirSync(ext);
-    }
-    moveFile(file, ext);
-}
-
-function handleDir(dir) {
-    console.log(` ${dir} is a directory`);
-    const files = fs.readdirSync(filename);
-    console.log(files);
-    for (let file of files) {
-        console.log(file);
-        if (fs.statSync(file).isDirectory()) {
-            console.log(file, ' - This is a directory');
-        } else {
-            handleFile(file);
-        }
-    }
-}
-
 function checkFileType(filename) { 
     if (fs.statSync(filename).isDirectory()) { 
         handleDir(filename);
@@ -51,13 +13,46 @@ function checkFileType(filename) {
     }
 }
 
+
+function handleFile(file) {
+    const ext = path.extname(file).replace('.', '');
+    if (!fs.existsSync(ext)) {
+        fs.mkdirSync(ext);
+        console.log(`Directory: "${ext}" has been created`);        
+    }
+    moveFile(file, ext);
+}
+
+
+function handleDir(dir) {
+    const files = fs.readdirSync(filename);
+    console.log(`Given file: ${dir} is a directory`);
+    console.log(files);
+    for (let file of files) {
+        if (fs.statSync(file).isFile()) {
+            handleFile(file);
+        }
+    }
+}
+
+
 function moveFile(file, targetDir) {
     const sourcePath = path.resolve(file);
     const sourceDir = path.dirname(sourcePath);
-    console.log(sourcePath);
-    console.log(sourceDir);
     const destPath = path.join(sourceDir, targetDir, file);
     fs.renameSync(sourcePath, destPath);
+    console.log(`  ${file}  -> ${targetDir}`);
 }
 
-checkFileType(filename);
+
+try {
+    if (!filename) {
+        throw Error('A file to sort must be specified!')  // 1 Handle if any file specified
+    }
+    if (!fs.existsSync(filename)) {
+        throw Error("Given path doesn't exists. Check the path");  // 2 Check if this path exists
+    }
+    checkFileType(filename);
+} catch (err) {
+    console.error('Error: ', err.message);
+}
